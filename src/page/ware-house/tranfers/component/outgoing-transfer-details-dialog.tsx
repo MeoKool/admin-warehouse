d3e7f82d2bf8bfbab51fb4d235e2b39a0126216c
MeoKool <truongsyquang1202@gmail.com>
2025-06-07 22:02:22 +0700

    feat: add 'canceled' status handling in status info for incoming and outgoing transfer dialogs

--- src/page/ware-house/tranfers/component/outgoing-transfer-details-dialog.tsx
+++ src/page/ware-house/tranfers/component/outgoing-transfer-details-dialog.tsx
@@ -75,6 +75,14 @@ export const getStatusInfo = (
       label: "Đã điều phối",
       icon: "alert-circle",
     };
+  } else if (statusLower === "canceled") {
+    return {
+      color: "text-red-800",
+      bgColor: "bg-red-100",
+      hoverColor: "hover:bg-red-200",
+      label: "Đã hủy",
+      icon: "alert-circle",
+    };
   } else {
     return {
       color: "text-gray-800",
